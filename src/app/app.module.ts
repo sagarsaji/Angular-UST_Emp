8e66f52375c4cd5cfbc636104767b15a9efbaa34
Sagar Saji <sagarsaji7@gmail.com>
2023-03-31 23:42:28 +0530

    march 31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeRegService } from './employee-reg.service';
+import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeRegService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee-reg/employee-reg.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { EmployeeRegService } from '../employee-reg.service';
+import { User } from '../User';
+
+@Component({
+  selector: 'app-employee-reg',
+  templateUrl: './employee-reg.component.html',
+  styleUrls: ['./employee-reg.component.css']
+})
+export class EmployeeRegComponent implements OnInit {
+
+  user:User = new User("","",0,"");
+  message: any;
+  constructor(private service:EmployeeRegService){
+    
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnChanges() {
+    
+}
+
+public registerNow(){
+  let resp = this.service.doRegistration(this.user);
+  resp.subscribe(data=>this.message=data);
+}
+
+}
